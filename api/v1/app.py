16f84718e8db157d6d58975a3a00c915288fc015
vivian347 <vkinyua001@gmail.com>
2023-01-24 02:00:24 +0300

    3

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" create an instance of Flask """
+from flask import Flask, Blueprint, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exceptions):
+    """ cleans up """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == ('__main__'):
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" creates route """
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ the status """
+    return jsonify({"status":"OK"})
+
