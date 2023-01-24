17ce20c1369b72d823673aa62e9be1af6ce8b128
vivian347 <vkinyua001@gmail.com>
2023-01-24 22:48:29 +0300

    4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,28 @@ from flask import Flask, Blueprint, jsonify
 from models import storage
 
 
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ the status """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ endpoint that retrieves no of each object """
+    ret_dict = {}
+    for key, val in classes.items():
+        ret_dict[key] = storage.count(val)
+    return jsonify(ret_dict)
+
+if __name__ == "__main__":
+    pass
